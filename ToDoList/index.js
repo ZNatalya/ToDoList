b08905ca3db814708a5f38d904edaddb52b0ec7f
ZNatalya <98710261+ZNatalya@users.noreply.github.com>
2023-08-29 20:35:29 +0300

    Начальный шаблон HTML + CSS

--- ToDoList/index.js
+++ ToDoList/index.js
@@ -1,16 +1,25 @@
 const list  = document.getElementById('todos');
 document.querySelector('button').addEventListener('click', handleClick);
+document.addEventListener('DOMContentLoaded', loadTodos);
 
 function handleClick() {
     const newTodo = this.previousElementSibling.value.trim();
 
     if (newTodo) {
         createTodo(newTodo);
+        saveToStorage(newTodo);
         this.previousElementSibling.value = "";
     }else {
         alert("Введите данные");
     }
 }
+// Сохраняем данные в локальной истории
+function saveToStorage(todo) {
+    const todos = JSON.parse(localStorage.getItem('tasks')) || [];
+
+    localStorage.setItem('tasks', JSON.stringify([...todos, todo]));
+}
+
 // создаем добавление
 function createTodo(text) {
     const li = document.createElement('li');
@@ -20,8 +29,16 @@ function createTodo(text) {
     list.append(li);
 }
 
+function loadTodos() {
+    const todos = JSON.parse(localStorage.getItem('tasks'));
+
+    if (todos) {
+        todos.forEach(todo => createTodo(todo));
+    }
+}
 // удаляем список
 function removeTodo() {
     this.removeEventListener('click', removeTodo); // удаляем клик
     this.remove(); // удаляем элемент на который кликали
 }
+
